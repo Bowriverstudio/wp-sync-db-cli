e8f20e58af68829f42782c61b411a5d29522c06f
Maurice Tadros <maurice.tadros@gmail.com>
2020-05-07 14:36:31 -0600

    Added more commands

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
 # WP Sync DB CLI
+
 An addon for [WP Sync DB](https://github.com/slang800/wp-sync-db) that allows you to execute migrations using a function call or via WP-CLI
 
 The plugin is to be installed as a standard WordPress plugin, ensure you have wp-sync-db installed as well and have WP-CLI installed on your server.
- Firstly, you'll want to set up a profile for the sync. So go ahead in your WP backend and set up the migration in WP Sync DB. Save the profile. You can have multiple profiles if you'd like.
- Profiles are numerical, saved in order of how they're listed in the backend.
- Commands for WP-CLI are:
-  ```wp wpsdb profiles```
+Firstly, you'll want to set up a profile for the sync. So go ahead in your WP backend and set up the migration in WP Sync DB. Save the profile. You can have multiple profiles if you'd like.
+Profiles are numerical, saved in order of how they're listed in the backend.
+Commands for WP-CLI are:
+`wp wpsdb profiles`
 
- ```wp wpsdb migrate [profile-number]```
- Example:
-```wp wpsdb migrate 1```
+`wp wpsdb migrate [profile-number]`
+Example:
+`wp wpsdb migrate 1`
 
+Added ability to toggle settings
+
+`wp wpsdb allow_push`
+`wp wpsdb disallow_push`
+
+`wp wpsdb allow_pull`
+`wp wpsdb disallow_pull`
+
+`wp wpsdb print_api_key`
+
+#Staging server settings typically:
+
+```
+git clone https://github.com/wp-sync-db/wp-sync-db.git
+git clone https://github.com/wp-sync-db/wp-sync-db-media-files.git
+git clone https://github.com/Bowriverstudio/wp-sync-db-cli.git
+
+
+```
--- class/command.php
+++ class/command.php
@@ -3,59 +3,181 @@
 /**
  * Migrate your DB using WP Sync DB.
  */
-class WPSDBCLI extends WP_CLI_Command {
+class WPSDBCLI extends WP_CLI_Command
+{
 
-	/**
-	 * Run a migration.
-	 *
-	 * ## OPTIONS
-	 *
-	 * <profile>
-	 * : ID of the profile to use for the migration.
-	 *
-	 * ## EXAMPLES
-	 *
-	 * 	wp wpsdb migrate 1
-	 *
-	 * @synopsis <profile>
-	 *
-	 * @since 1.0
-	 */
-	public function migrate( $args, $assoc_args ) {
-		$profile = $args[0];
+    /**
+     * Run a migration.
+     *
+     * ## OPTIONS
+     *
+     * <profile>
+     * : ID of the profile to use for the migration.
+     *
+     * ## EXAMPLES
+     *
+     *     wp wpsdb migrate 1
+     *
+     * @synopsis <profile>
+     *
+     * @since 1.0
+     */
+    public function migrate($args, $assoc_args)
+    {
+        $profile = $args[0];
 
-		$result = wpsdb_migrate( $profile );
+        $result = wpsdb_migrate($profile);
 
-		if ( true === $result ) {
-			WP_CLI::success( __( 'Migration successful.', 'wp-sync-db-cli' ) );
-			return;
-		}
+        if (true === $result) {
+            WP_CLI::success(__('Migration successful.', 'wp-sync-db-cli'));
+            return;
+        }
 
-		WP_CLI::warning( $result->get_error_message() );
-		return;
-	}
+        WP_CLI::warning($result->get_error_message());
+        return;
+    }
 
     /**
      * List all profiles.
      *
      * ## EXAMPLES
      *
-     * 	wp wpsdb profiles
+     *     wp wpsdb profiles
      *
      * @since 1.1 - Changed in git pull
      *
      */
-    public function profiles() {
+    public function profiles()
+    {
         $result = wpsdb_profiles();
-        if ( $result ) {
-            WP_CLI::log( __( $result, 'wp-sync-db-cli' ) );
+        if ($result) {
+            WP_CLI::log(__($result, 'wp-sync-db-cli'));
             return;
         }
-        WP_CLI::warning( __( 'You have no profiles.', 'wp-sync-db-cli' ) );
+        WP_CLI::warning(__('You have no profiles.', 'wp-sync-db-cli'));
         return;
     }
 
+    /**
+     * Sets the "Allow Pull" option to true
+     *
+     * ## EXAMPLES
+     *
+     *     wp wpsdb allow
+     *
+     * @since 1.2
+     *
+     */
+    public function display_options()
+    {
+        $option = get_option('wpsdb_settings');
+        if (function_exists('d')) {
+            d($option);
+        } else {
+            var_dump($option);
+        }
+    }
+    /**
+     * Sets the "Allow Pull" option to true
+     *
+     * ## EXAMPLES
+     *
+     *     wp wpsdb allow_pull
+     *
+     * @since 1.2
+     *
+     */
+    public function allow_pull()
+    {
+        $this->update_option('allow_pull', true);
+        $this->display_options();
+        return;
+    }
+
+    /**
+     * Sets the "Allow Pull" option to false
+     *
+     * ## EXAMPLES
+     *
+     *     wp wpsdb disallow_pull
+     *
+     * @since 1.2
+     *
+     */
+
+    public function disallow_pull()
+    {
+        $this->update_option('allow_pull', false);
+        $this->display_options();
+        return;
+    }
+
+    /**
+     * Sets the "Allow Push" option to true
+     *
+     * ## EXAMPLES
+     *
+     *     wp wpsdb allow_push
+     *
+     * @since 1.2
+     *
+     */
+    public function allow_push()
+    {
+        $this->update_option('allow_push', true);
+        $this->display_options();
+        return;
+    }
+
+    /**
+     * Sets the "Allow Push" option to false
+     *
+     * ## EXAMPLES
+     *
+     *     wp wpsdb disallow_push
+     *
+     * @since 1.2
+     *
+     */
+
+    public function disallow_push()
+    {
+        $this->update_option('allow_push', false);
+        $this->display_options();
+        return;
+    }
+
+    /**
+     * Sets the "Allow Push" option to false
+     *
+     * ## EXAMPLES
+     *
+     *     wp wpsdb print_api_key
+     *
+     * @since 1.2
+     *
+     */
+
+    public function print_api_key()
+    {
+        $option = get_option('wpsdb_settings');
+        WP_CLI::success(__('API Key is:'));
+        WP_CLI::success(__(site_url() . '/' . $option['key']));
+
+        return;
+    }
+
+    private function update_option($field, $value)
+    {
+        $option = get_option('wpsdb_settings');
+        if (!$option) {
+            WP_CLI::warning(__('wpsdb_settings does not exist'));
+            return;
+        }
+        $option[$field] = $value;
+        update_option('wpsdb_settings', $option);
+    }
 
 }
 
-WP_CLI::add_command( 'wpsdb', 'WPSDBCLI' );
+WP_CLI::add_command('wpsdb', 'WPSDBCLI');
